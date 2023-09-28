084bd2287a2299d2ec126d3a3575a0ba04d2a82a
ozzy ozbourne <khanosaid726@gmail.com>
2023-09-29 02:53:37 +0530

    memory leak fixed

--- all_subsequences.zig
+++ all_subsequences.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const allocator = std.testing.allocator;
-var d2= std.ArrayList(std.ArrayList(isize)).init(allocator);
+var d2 = std.ArrayList(std.ArrayList(isize)).init(allocator);
 
 
 pub fn main()void {
@@ -10,16 +10,14 @@ pub fn main()void {
 
 fn generate_all_subsequences(arr: [] const isize,  i: usize, d1: *std.ArrayList(isize)) !void {
     if(arr.len == i){
-      var copy_d1 = std.ArrayList(isize).init(allocator);
-      for (d1.items) |j| { try copy_d1.append(j);}
-      try d2.append(copy_d1);
+      try d2.append(try d1.clone());
       return;
     }
     try d1.append(arr[i]);
     try generate_all_subsequences(arr, i+1, d1);
     _= d1.pop();
     try generate_all_subsequences(arr, i+1, d1);
-
+ 
 }
 
 fn print_subsequences() void{
@@ -35,11 +33,14 @@ test "Zig Memory check" {
     defer d2.deinit();
 
     var d1 = std.ArrayList(isize).init(allocator);
-    d1.deinit();
+    defer d1.deinit();
 
     const v = [_] isize{1, 2, 3};
     try generate_all_subsequences(v[0..v.len], 0, &d1);
     print_subsequences();
    
     try std.testing.expect(d2.items.len == 8);
+    for (d2.items) |item| {
+        item.deinit();
+    }
 }
